d99733f2b21871d404c592641701368427eff8cf
ut23405 <ut23405@gmail.com>
2021-07-04 03:15:13 +0900

    socket-sample

---
+++ con11/server1.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.Text.RegularExpressions
+
+Module Module1
+	'ソケット通信(サーバー側)
+	Sub Main()
+		Dim host1 As IPAddress = IPAddress.Parse("127.0.0.1")
+		Dim port1 As Integer = 8765
+		Dim ipe1 As IPEndPoint = New IPEndPoint(host1, port1)
+		Dim server As TcpListener = Nothing
+		Dim recvline As String
+		Dim sendline As String = Nothing
+		Dim num As Integer
+		Dim outflg As Boolean = False
+		Dim buf(1024) As Byte
+		Dim reg As Regex = New Regex("\0")
+		Try
+			server = New TcpListener(ipe1)
+			Console.WriteLine("クライアントからの入力待ち状態")
+			server.Start()
+			While (True)
+				Using client = server.AcceptTcpClient()
+					Using stream = client.GetStream()
+						While stream.Read(buf, 0, buf.Length) <> 0
+							recvline = reg.Replace(Encoding.UTF8.GetString(buf), "")
+							Console.WriteLine("client側の入力文字＝" + recvline)
+							If recvline = "bye" Then
+								outflg = True
+								Exit While
+							End If
+							Try
+								num = Integer.Parse(recvline)
+								If num Mod 2 = 0 Then
+									sendline = "OKです"
+								Else
+									sendline = "NGです"
+								End If
+							Catch ex As Exception
+								sendline = "数値を入力して下さい"
+							Finally
+								buf = Encoding.UTF8.GetBytes(sendline)
+								stream.Write(buf, 0, buf.Length)
+								Array.Clear(buf, 0, buf.Length)
+							End Try
+						End While
+						If (outflg = True) Then
+							Exit While
+						End If
+					End Using
+				End Using
+			End While
+		Catch ex As Exception
+			Console.WriteLine(ex.Message)
+		Finally
+			server.Stop()
+			Console.WriteLine("サーバー側終了です")
+		End Try
+	End Sub
+End Module
---
+++ con12/client1.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.Text.RegularExpressions
+
+Module client1
+	'ソケット通信(クライアント側)
+	Sub Main()
+		Dim host1 As IPAddress = IPAddress.Parse("127.0.0.1")
+		Dim port1 As Integer = 8765
+		Dim ipe1 As IPEndPoint = New IPEndPoint(host1, port1)
+		Dim line As String = Nothing
+		Dim buf1(1024) As Byte
+		Dim buf2(1024) As Byte
+		Dim reg As Regex = New Regex("\0")
+		Try
+			Using client = New TcpClient()
+				client.Connect(ipe1)
+				Using stream = client.GetStream()
+					While (line <> "bye")
+						' 標準入力からデータを取得
+						Console.WriteLine("--------------------------")
+						Console.WriteLine("偶数の数値を入力して下さい")
+						Console.WriteLine("--------------------------")
+						' サーバに送信
+						line = Console.ReadLine()
+						buf1 = Encoding.UTF8.GetBytes(line)
+						stream.Write(buf1, 0, buf1.Length)
+						' サーバから受信
+						stream.Read(buf2, 0, buf2.Length)
+						Console.WriteLine(
+							reg.Replace(Encoding.UTF8.GetString(buf2), ""))
+					End While
+				End Using
+			End Using
+		Catch ex As Exception
+			Console.WriteLine(ex.Message)
+		Finally
+			Console.WriteLine("クライアント側終了です")
+		End Try
+	End Sub
+End Module
